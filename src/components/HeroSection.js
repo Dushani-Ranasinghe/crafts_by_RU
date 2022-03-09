26cc29f5a6e7a9efe50e11b2e804f0eef78c9f9f
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-09 23:46:48 +0530

    Created Crousel in hero section and added images

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -15,44 +15,101 @@ import sixth from "../Resources/6.jpeg"
 function HeroSection() {
   return (
     <div className='HeroSection'>
-       <Carousel>
+        <div className='Carousel-container'>
+
+<Carousel>
+
   <Carousel.Item>
     <img
       className="d-block w-100"
       src={MainImg}
       alt="First slide"
-      width="600px"
-    />
+      height={500}/>
     <Carousel.Caption>
       <h3>First slide label</h3>
       <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
     </Carousel.Caption>
   </Carousel.Item>
+
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Second slide&bg=282c34"
+      src={first}
       alt="Second slide"
+      height={500}
     />
-
     <Carousel.Caption>
       <h3>Second slide label</h3>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
     </Carousel.Caption>
   </Carousel.Item>
+
   <Carousel.Item>
     <img
       className="d-block w-100"
-      src="holder.js/800x400?text=Third slide&bg=20232a"
+      src={second}
       alt="Third slide"
+      height={500}
     />
-
     <Carousel.Caption>
       <h3>Third slide label</h3>
       <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
     </Carousel.Caption>
   </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={third}
+      alt="Third slide"
+      height={500}
+    />
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={fourth}
+      alt="Third slide"
+      height={500}
+    />
+    <Carousel.Caption>
+      <h3>fourth slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={fifth}
+      alt="Third slide"
+      height={500}
+    />
+    <Carousel.Caption>
+      <h3>fifth slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+
+  <Carousel.Item>
+    <img
+      className="d-block w-100"
+      src={sixth}
+      alt="Third slide"
+      height={500}
+    />
+    <Carousel.Caption>
+      <h3>sixth slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
 </Carousel>
+</div>
     </div>
   )
 }
