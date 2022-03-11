d528d031d3a68c28460cb3a7931043bba476a3e9
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-11 23:58:19 +0530

    Created sub navgation component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import Body from './components/Body';
+import SubNavbar from './components/SubNavbar';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
     <div className='App-container'>
      <Navbar/>
      <Body/>
+     <SubNavbar/>
      </div>
     </div>
   );
---
+++ src/components/SubNavbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SubNavbar() {
+  return (
+    <div className='SubNavbar'>
+        <div className='Sub-Nav-Links'>
+         <a href='/home'>ALL</a>
+         <a href='/gift_box'>GIFT BOX</a>
+         <a href='/explosion_box'>EXPLOSION BOX</a>
+         <a href='/gift_card'>GIFT CARD</a>
+         <a href='/gift_basket'>GIFT BASKET</a>
+         <a href='/gift_jar'>GIFT JAR</a>
+         <a href='/event_decor'>EVENT DECORATIONS</a>
+      </div>
+    </div>
+  )
+}
+
+export default SubNavbar
