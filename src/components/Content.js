963f5cd92b28c004124b925b6aae18253b1c310e
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-22 17:09:00 +0530

    Added Card details and imported images

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,41 +1,127 @@
-import React from 'react'
-import "../Styles/Content.css"
-import BgImg from "../Resources/content-bg.png"
-import GiftBox from "../Resources/Products/giftbox.png"
-import ExpBox from "../Resources/Products/explosionbox.png"
-import GiftJar from "../Resources/Products/giftjar.png"
+import React from "react";
+import "../Styles/Content.css";
+import BgImg from "../Resources/content-bg.png";
+import GiftBox from "../Resources/Products/giftbox.png";
+import ExpBox from "../Resources/Products/explosionbox.png";
+import GiftJar from "../Resources/Products/giftjar.png";
+import GiftCard from "../Resources/Products/giftcard.png"
+import GiftBasket from "../Resources/Products/giftbasket.png"
+import Party from "../Resources/Products/party.png"
 
 function Content() {
   return (
     <div>
-        <div className='parent'>
-        <img src={BgImg} alt="background" className='image2' width={200} height={261}/>
-
-        <div className='row1'>
-
-            <div className='card'>
-            <img src={GiftBox} alt="giftbox" className='image1' width={200} height={261}/>
+      <div className="parent">
+        <img
+          src={BgImg}
+          alt="background"
+          className="image2"
+          width={200}
+          height={261}
+        />
+          <img
+          src={BgImg}
+          alt="background"
+          className="image3"
+          width={200}
+          height={261}
+        />
+        <div className="row1">
+          <div className="card">
+            <img
+              src={GiftBox}
+              alt="giftbox"
+              className="image1"
+              width={200}
+              height={261}
+            />
             <h1>GIFT BOXES</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus vitae amet aliquet diam.</p>
-           </div>
-        
-            <div className='card'>
-            <img src={ExpBox} alt="giftbox" className='image1' width={200} height={261}/>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              vitae amet aliquet diam.
+            </p>
+          </div>
+
+          <div className="card">
+            <img
+              src={ExpBox}
+              alt="giftbox"
+              className="image1"
+              width={200}
+              height={261}
+            />
             <h1>EXPLOSION BOXES</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus vitae amet aliquet diam.</p>
-            </div>
-       
-            <div className='card'>
-            <img src={GiftJar} alt="giftbox" className='image1' width={200} height={261}/>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              vitae amet aliquet diam.
+            </p>
+          </div>
+
+          <div className="card">
+            <img
+              src={GiftJar}
+              alt="giftbox"
+              className="image1"
+              width={200}
+              height={261}
+            />
             <h1>GIFT JARS</h1>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus vitae amet aliquet diam.</p>
-            </div>
-       
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              vitae amet aliquet diam.
+            </p>
+          </div>
         </div>
-       
+        <div className="row1">
+          <div className="card">
+            <img
+              src={GiftCard}
+              alt="giftbox"
+              className="image1"
+              width={200}
+              height={261}
+            />
+            <h1>GIFT CARDS</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              vitae amet aliquet diam.
+            </p>
+          </div>
+
+          <div className="card">
+            <img
+              src={GiftBasket}
+              alt="giftbox"
+              className="image1"
+              width={200}
+              height={261}
+            />
+            <h1>GIFT BASKETS</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              vitae amet aliquet diam.
+            </p>
+          </div>
+
+          <div className="card">
+            <img
+              src={Party}
+              alt="giftbox"
+              className="image1"
+              width={200}
+              height={261}
+            />
+            <h1>EVENT PLANNING</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus
+              vitae amet aliquet diam.
+            </p>
+          </div>
         </div>
+      
+      </div>
     </div>
-  )
+  );
 }
 
-export default Content
+export default Content;
