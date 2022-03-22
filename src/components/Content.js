361c166b84c6d69d10da3a9db78369c3bfc9ee42
Dushani-Ranasinghe <uththaradushani@gmail.com>
2022-03-22 15:41:54 +0530

    Created content cards and styled them

--- src/components/Content.js
+++ src/components/Content.js
@@ -10,10 +10,27 @@ function Content() {
     <div>
         <div className='parent'>
         <img src={BgImg} alt="background" className='image2' width={200} height={261}/>
+
         <div className='row1'>
-        <img src={GiftBox} alt="giftbox" className='image1' width={200} height={261}/>
-        <img src={ExpBox} alt="giftbox" className='image1' width={200} height={261}/>
-        <img src={GiftJar} alt="giftbox" className='image1' width={200} height={261}/>
+
+            <div className='card'>
+            <img src={GiftBox} alt="giftbox" className='image1' width={200} height={261}/>
+            <h1>GIFT BOXES</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus vitae amet aliquet diam.</p>
+           </div>
+        
+            <div className='card'>
+            <img src={ExpBox} alt="giftbox" className='image1' width={200} height={261}/>
+            <h1>EXPLOSION BOXES</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus vitae amet aliquet diam.</p>
+            </div>
+       
+            <div className='card'>
+            <img src={GiftJar} alt="giftbox" className='image1' width={200} height={261}/>
+            <h1>GIFT JARS</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Faucibus vitae amet aliquet diam.</p>
+            </div>
+       
         </div>
        
         </div>
